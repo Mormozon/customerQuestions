6df472720016ac921e910d3cb17a7389f1001291
Philip So <philtheso@gmail.com>
2018-07-30 17:23:42 -0700

    Restructured schemas

--- database/models.js
+++ database/models.js
@@ -1,12 +1,11 @@
 const Sequelize = require('sequelize');
 const sequelize = require('./index');
 
-const User = sequelize.define('user', {
-  username: {type: Sequelize.STRING}
-});
-
 const Product = sequelize.define('product', {
-  name: {type: Sequelize.STRING}
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
 });
 
 const Question = sequelize.define('question', {
@@ -25,13 +24,22 @@ const Answer = sequelize.define('answer', {
     type: Sequelize.STRING,
     allowNull: false
   },
+  username: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
   date: {
-    type: Sequelize.DATE
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW
   }
 });
+
+Question.belongsTo(Product);
+Product.hasMany(Question, {as: 'Questions'});
 
 Answer.belongsTo(Question);
 Question.hasMany(Answer, {as: 'Answers'});
 
+sequelize.sync({force: false});
 
 module.exports = { User, Product, Question, Answer};
