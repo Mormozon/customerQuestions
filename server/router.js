778dac5ecbfc3fb78dca0157305861cfa317a845
Philip So <philtheso@gmail.com>
2018-07-30 17:31:00 -0700

    Removed unnecessary routes

--- server/router.js
+++ server/router.js
@@ -1,20 +1,14 @@
 const router = require('express').Router();
 const controller = require('./controller');
 
-router.route('/user')
-  .get(controller.getUser)
-  .post(controller.postUser);
-
 router.route('/product')
   .get(controller.getProduct)
   .post(controller.postProduct);
 
 router.route('/question')
-  .get(controller.getQuestion)
   .post(controller.postQuestion);
 
 router.route('/answer')
-  .get(controller.getAnswer)
   .post(controller.postAnswer);
 
 module.exports = router;
