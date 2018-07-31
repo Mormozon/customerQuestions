51ac63519208c714fe3452e223113e6e2a916d04
Philip So <philtheso@gmail.com>
2018-07-31 08:17:46 -0700

    Controller mostly complete

--- server/controller.js
+++ server/controller.js
@@ -1,14 +1,58 @@
-const { User, Product, Question, Answer} = require ('../database/models');
+const { Product, Question, Answer } = require ('../database/models');
 
 const controller = {
-  get: (req, res) => {
+  getProduct: (req, res) => {
+    const { productName } = req.query;
 
+    Product.find({
+      name: productName
+    }, (err, data) => {
+      if (err) {console.error(err)}
+      else {res.status(200).send(data)}
+    });
   },
-  post: (req, res) => {
+  getQuestion: (req, res) => {
+    console.log(req.query);
+    const { question } = req.query;
 
+    Question.find({
+      question: question
+    }, (err, data) => {
+      if (err) {console.error(err)}
+      else {res.status(200).send(data)}
+    });
   },
-  delete: (req, res) => {
+  postQuestion: (req, res) => {
+    const { question } = req.body;
 
+    new Question({
+      question: question
+    }).save((err, data) => {
+      if (err) {console.error(err)}
+      else {res.status(201).send(data)}
+    });
+  },
+  getAnswer: (req, res) => {
+    console.log(req.query);
+    const { answer } = req.query;
+
+    Answer.find({
+      answer: answer
+    }, (err, data) => {
+      if (err) {console.error(err)}
+      else {res.status(200).send(data)}
+    });
+  },
+  postAnswer: (req, res) => {
+    const { answer, username } = req.body;
+
+    Answer.create({
+      answer: answer,
+      username: username
+    }, (err, data) => {
+      if (err) {console.error(err)}
+      else {res.status(201).send(data)}
+    });
   }
 }
 
