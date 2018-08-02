c179cea2f1463fe97bd9022bc9714e43a90a6d96
Philip So <philtheso@gmail.com>
2018-08-02 10:21:23 -0700

    Added search bar logo

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import styles from './styles/search.css';
 
 const Search = () => (
-  <div className={ styles.searchbar } >
+  <div className={ styles.searchbar }>
+    <div className={ styles.searchLogo }></div>
     <input className={ styles.searchinput }type="search" maxlength="150" placeholder="Have a question? Search for answers"/>
   </div>
 );
